c18a2958de79434adcc0dc1d3be2a5ee719a0cc4
Flavien ROUSSEL <froussel@e2r3p20.42.fr>
2020-10-17 14:55:19 +0200

    patch app.js

--- rails/srcs/config/routes.rb
+++ rails/srcs/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-	root to: 'pages#live'
+	root to: 'pages#sign-in'
 
 	get '/live', to: 'pages#live', as: 'live'
 	get '/play', to: 'pages#play', as: 'play'

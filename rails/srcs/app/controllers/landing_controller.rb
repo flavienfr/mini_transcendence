ff47692e488ac44a4ab43991b6f3c9e6a5d67fcf
mad221 <maximepouzol@gmail.com>
2020-10-22 23:51:42 +0200

    correction landing controller redirect_to root_path not landings_path

--- rails/srcs/app/controllers/landing_controller.rb
+++ rails/srcs/app/controllers/landing_controller.rb
@@ -98,7 +98,7 @@ class LandingController < ApplicationController
       puts user.to_yaml
 
       # ocker-compose run web rails console -> to see if it worked
-      redirect_to landings_path # à enlever pcq ça reload la page
+      redirect_to root_path # à enlever pcq ça reload la page
 
     end
 

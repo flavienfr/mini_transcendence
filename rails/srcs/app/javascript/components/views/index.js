d4963fe0abd51438a151b61848cea8cbd4a92dfa
flavienfr <flav.rsl@hotmail.fr>
2020-10-23 16:05:18 +0200

    youri

--- rails/srcs/app/javascript/components/views/index.js
+++ rails/srcs/app/javascript/components/views/index.js
@@ -221,11 +221,12 @@ export var TournamentView = Backbone.View.extend({
 export var WarView = Backbone.View.extend({
 	el : '#inside-page',
 	template: _.template($('#war-template').html()),
+	template_guilds: _.template($('#war-guild-template').html()),
 	collection: guilds,
-	model: guild_1,
+	model: null,
 
 	initialize : function() {
-		console.log("WarView created");
+		this.model= this.collection.first();
 	},
 	events: {
 		"change #guild_drop_down": "render_guild_info"
@@ -235,12 +236,14 @@ export var WarView = Backbone.View.extend({
 		var guild_to_print = this.collection.findWhere({name: guild_name});
 		console.log("change", guild_name);
 		console.log("model", guild_to_print.attributes);
-		//DOM to change value
+		this.model = guild_to_print;
+		this.$('.war_pannel_header').html(this.template_guilds(this.model.toJSON()));
 	},
 	render : function() {
-		// Try do modify template before rendering 
 		this.$el.html(this.template(this.model.toJSON()));
+		this.$('.war_pannel_header').html(this.template_guilds(this.model.toJSON()));
 
+		// Try do modify template before rendering
 		_.each(this.collection.toJSON(), function(model){
 			$('#guild_drop_down').append(
 				$('<option></option>').html(model.name)
@@ -250,3 +253,43 @@ export var WarView = Backbone.View.extend({
 		return this;
 	}
 });
+
+/*
+export var WarView = Backbone.View.extend({
+	el : '#inside-page',
+	template: _.template($('#war-template').html()),
+	//template_guilds: _.template($('#war-guild-template').html()),
+	collection: guilds,
+	model: null,
+
+	initialize : function() {
+		this.model= this.collection.first();
+	},
+	events: {
+		"change #guild_drop_down": "render_guild_info"
+	},
+	render_guild_info : function() {
+		var guild_name = $("#guild_drop_down option:selected").text();
+		var guild_to_print = this.collection.findWhere({name: guild_name});
+		console.log("change", guild_name);
+		console.log("model", guild_to_print.attributes);
+		this.model = guild_to_print;
+		//this.render();
+
+		//DOM to change value
+	},
+	render : function() {
+		console.log("model render:", this.model.attributes);
+		// Try do modify template before rendering 
+		this.$el.html(this.template());
+		//this.$('.war_pannel_header').html(this.template_guilds(this.model.toJSON()));
+
+		_.each(this.collection.toJSON(), function(model){
+			$('#guild_drop_down').append(
+				$('<option></option>').html(model.name)
+			);
+		});
+
+		return this;
+	}
+});*/

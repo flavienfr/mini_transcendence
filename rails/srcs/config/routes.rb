240d642fd90dfb486a8cb677cd2d4544698d9901
flavienfr <flav.rsl@hotmail.fr>
2020-10-26 19:32:01 +0100

    get owner id

--- rails/srcs/config/routes.rb
+++ rails/srcs/config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
-	  
-  resources :tournaments
-  resources :ask_for_games
-  resources :game_participations
-  resources :games
-  resources :messages
-  resources :channel_participations
-  resources :channels
-  resources :user_achievements
-  resources :achievements
-  resources :friendships
-  resources :guild_participations
-  resources :guilds
+
+	get 'guilds/get_guild_owner' => 'guilds#get_guild_owner'
+  
+  	resources :tournaments
+  	resources :ask_for_games
+  	resources :game_participations
+  	resources :games
+  	resources :messages
+  	resources :channel_participations
+  	resources :channels
+  	resources :user_achievements
+  	resources :achievements
+  	resources :friendships
+  	resources :guild_participations
+  	resources :guilds
 	resources :sessions
 	
 	resources :users
--- rails/srcs/db/seeds.rb
+++ rails/srcs/db/seeds.rb
@@ -7,15 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # destroy all objects
+
+
 GuildParticipation.destroy_all
 Guild.destroy_all
 Friendship.destroy_all
+Session.destroy_all
 User.destroy_all
 
 # users
+
 francis = User.create(name: "francis", avatar: "https://cdn.intra.42.fr/users/fberger.jpg", current_status: "", points: 0, is_admin: false) 
-yamin = User.create(name: "yamin", avatar: "https://cdn.intra.42.fr/users/ylegzoul.jpg", current_status: "", points: 0, is_admin: false) 
-flavien = User.create(name: "flavien", avatar: "https://cdn.intra.42.fr/users/froussel.jpg", current_status: "", points: 0, is_admin: false) 
+yamin = User.create(name: "yamin", avatar: "https://cdn.intra.42.fr/users/ylegzoul.jpg", current_status: "", points: 0, is_admin: false)
+flavien = User.create(name: "flavien", avatar: "https://cdn.intra.42.fr/users/froussel.jpg", current_status: "", points: 0, is_admin: false)
 luc = User.create(name: "luc", avatar: "https://cdn.intra.42.fr/users/lhuang.jpg", current_status: "", points: 0, is_admin: false) 
 maxime = User.create(name: "maxime", avatar: "https://cdn.intra.42.fr/users/mpouzol.jpg", current_status: "", points: 0, is_admin: false) 
 
@@ -30,14 +34,15 @@ f3 = Friendship.create(user1_id: francis.id, user2_id: luc.id, status: "active")
 f4 = Friendship.create(user1_id: francis.id, user2_id: maxime.id, status: "active")
 
 # guilds
-assemblee = Guild.create(name: "The Assemblee", anagram: "42", points: 0, is_making_war: false ,owner_id: francis.id)
-order = Guild.create(name: "The Order", anagram: "42", points: 0, is_making_war: false, owner_id: yamin.id)
+assemblee = Guild.create(name: "The Assemblee", anagram: "42", points: 5412, is_making_war: false ,owner_id: francis.id)
+order = Guild.create(name: "The Order", anagram: "42", points: 1235, is_making_war: false, owner_id: yamin.id)
 
 # guild participations
 # Assemblee
 ap1 = GuildParticipation.create(user_id: francis.id, guild_id: assemblee.id, is_admin: true, is_officer: false)
 ap2 = GuildParticipation.create(user_id: flavien.id, guild_id: assemblee.id, is_admin: false, is_officer: true)
 # Order
+
 op1 = GuildParticipation.create(user_id: yamin.id, guild_id: order.id, is_admin: true, is_officer: false)
 op2 = GuildParticipation.create(user_id: luc.id, guild_id: order.id, is_admin: false, is_officer: true)
 op3 = GuildParticipation.create(user_id: maxime.id, guild_id: order.id, is_admin: false, is_officer: true)

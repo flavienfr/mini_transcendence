116439850483179eb448b8ec2f9156e15c4548df
lhuang <lhuang@student.42.fr>
2020-10-29 15:37:25 +0100

    fix create group button in live page

--- rails/srcs/app/javascript/components/views/play.js
+++ rails/srcs/app/javascript/components/views/play.js
@@ -10,7 +10,7 @@ export var PlayView = Backbone.View.extend({
 
 	render : function() {
 		this.$el.html(this.template);
-		chatview.setElement("#play_chat_area").render_chat_main(); // https://solvemprobler.com/blog/2013/04/07/backbone-tips-rendering-views-and-their-childviews/
+		chatview.setElement("#chat_area").render_chat_main(); // https://solvemprobler.com/blog/2013/04/07/backbone-tips-rendering-views-and-their-childviews/
 		console.log("Render Play");
 		return this;
 	}

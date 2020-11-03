0d8c514190ae14362a5425e34713f905d9468064
flavienfr <flav.rsl@hotmail.fr>
2020-11-03 14:07:09 +0100

    froussel branch

--- rails/srcs/entrypoint.sh
+++ rails/srcs/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
-# yarn install --check-files
+yarn install --check-files
 rails db:create
 rails db:migrate
 

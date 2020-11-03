1eb5a3008f2df96ae02038e92548638fb3f861e8
mad221 <maximepouzol@gmail.com>
2020-11-03 17:17:38 +0100

    better

--- rails/srcs/app/channels/pongnot_channel.rb
+++ rails/srcs/app/channels/pongnot_channel.rb
@@ -1,9 +1,11 @@
 class PongnotChannel < ApplicationCable::Channel
   def subscribed
+    puts("new subscribe")
     stream_from "pongnot_channel_#{params[:pong_id]}"
   end
 
   def receive(data)
+  puts("reveive = ");
   puts (data);    
   ActionCable.server.broadcast("pongnot_channel_#{params[:pong_id]}", data);
   end

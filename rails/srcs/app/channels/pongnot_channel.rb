83f8c0e40a3ba9f4a7c2d00de1c645a3fa684948
mad221 <maximepouzol@gmail.com>
2020-11-03 20:27:35 +0100

    en attente

--- rails/srcs/app/channels/pongnot_channel.rb
+++ rails/srcs/app/channels/pongnot_channel.rb
@@ -1,13 +1,10 @@
 class PongnotChannel < ApplicationCable::Channel
   def subscribed
-    puts("new subscribe")
     stream_from "pongnot_channel_#{params[:pong_id]}"
   end
 
   def receive(data)
-  puts("reveive = ");
-  puts (data);    
-  ActionCable.server.broadcast("pongnot_channel_#{params[:pong_id]}", data);
+    ActionCable.server.broadcast("pongnot_channel_#{params[:pong_id]}", data);
   end
 
   def unsubscribed

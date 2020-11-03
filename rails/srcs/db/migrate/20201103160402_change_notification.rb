907f72d021dcd8ad020d0a38394d27fa27d52d3e
Fraberg <frcs.berger@gmail.com>
2020-11-03 18:53:20 +0100

    updated migrations cmds

--- rails/srcs/db/migrate/20201103160402_change_notification.rb
+++ rails/srcs/db/migrate/20201103160402_change_notification.rb
@@ -4,6 +4,6 @@ class ChangeNotification < ActiveRecord::Migration[6.0]
     add_column :notifications, :user_id , :integer, :references => "users"
     add_column :notifications, :table_id, :integer
     remove_column :notifications, :type
-    rename_column :notifications, :table_type, :string
+    add_column :notifications, :table_type, :string
   end
 end

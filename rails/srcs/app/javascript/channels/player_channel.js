abc892a989efc6d865fb628dee11bb5c299773f5
mad221 <maximepouzol@gmail.com>
2020-11-15 16:55:13 +0100

    correctif yamin part + protection live view

--- rails/srcs/app/javascript/channels/player_channel.js
+++ rails/srcs/app/javascript/channels/player_channel.js
@@ -9,6 +9,7 @@ consumer.subscriptions.create("PlayerChannel", {
     // Called when the subscription has been terminated by the server
   },
 
+
   async received(data) {
     await playercollection.fetch();
     var list = playercollection.toJSON();
@@ -20,27 +21,24 @@ consumer.subscriptions.create("PlayerChannel", {
       }
       if (playercollection.length == 2 && list[0].user_id == current_user_id)
       {
-        var ask_for_game = new Ask_for_game({from_user_id: list[0].user_id, to_user_id: list[1].user_id, type: "ladder_match_making" });
-        ask_for_game.save(ask_for_game, {
-          success: function() {
-           },
-          error: function() {
-            return ;
-          },
-          wait: true
-        });
-        socket_notif.send(JSON.stringify({
+        // var ask_for_game = new Ask_for_game({from_user_id: list[0].user_id, to_user_id: list[1].user_id, type: "ladder_match_making" });
+        var AskForGame = Backbone.Model.extend({
+          urlRoot: '/ask_for_games'
+        })
+        var ask_for_game = new AskForGame();
+        ask_for_game.save({ask_for_game:{from_user_id: list[1].user_id, to_user_id: list[0].user_id, status: 'playing', game_type: "ladder_match_making"}});
+        await socket_notif.send(JSON.stringify({
             command: "message",
             identifier: JSON.stringify({
-            channel: "PongnotChannel",
-            pong_id: 0
+              channel: "PongnotChannel",
+              pong_id: 0
             }),
             data: JSON.stringify({
                 data: {play: "true", id_host: list[0].user_id, id_guest: list[1].user_id, ask_id: ask_for_game.id}  // send a message to the guest for play with host, it s send his id et the id of the guest
             })
         }));
-        playview.render();
-        setTimeout(() => {pongguest.setElement("#pong-area").render();}, 800);  
+        await playview.render();
+        await pongguest.setElement("#pong-area").render();  
         var tmp = playercollection.findWhere({user_id: list[0].user_id});
         await tmp.destroy();
         tmp = playercollection.findWhere({user_id: list[1].user_id});

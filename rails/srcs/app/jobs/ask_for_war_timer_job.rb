749e80946713aef325eb596ae25ebc5d00ccda0a
Fraberg <frcs.berger@gmail.com>
2020-11-16 22:16:42 +0100

    ok active job

--- rails/srcs/app/jobs/ask_for_war_timer_job.rb
+++ rails/srcs/app/jobs/ask_for_war_timer_job.rb
@@ -1,16 +1,27 @@
 class AskForWarTimerJob < ApplicationJob
   queue_as :default
 
-  rescue_from() do |exception|
-    # Do something if the job generate an exception
-    puts "hello from AskForWarTimerJob => exception"
-  end
-
   def perform(ask_for_war_id)
     puts "hello from AskForWarTimerJob, ask_for_war_id = #{ask_for_war_id}"
+  
+    # begin
+    #   
+    # rescue => e
+    #   puts "exception: #{e}"
+    # end
+
     # Do something later
+    #
+    #
+    #
+
   end
 end
 
+# examples
 
-# AskForWarTimerJob.set(wait: 2.seconds).perform_later(1)
+# wait: x.time for now
+  # AskForWarTimerJob.set(wait: 2.seconds).perform_later(1)
+  # AskForWarTimerJob.set(wait: 2.hours).perform_later(1)
+
+# wait_until: Date.tomorrow.noon

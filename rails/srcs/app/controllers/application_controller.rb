1c6470c627d27eff8f59e5fae35db9814675120b
flavienfr <flav.rsl@hotmail.fr>
2020-11-17 05:36:48 +0100

    value check in ask for game

--- rails/srcs/app/controllers/application_controller.rb
+++ rails/srcs/app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user
+	helper_method :current_user
+	
+	#@toto = "le gros bide"
 
     def current_user
 
@@ -13,7 +15,7 @@ class ApplicationController < ActionController::Base
             #il faudra rajouter un moyen pour logout quand session plus valide timeout ? meme si c est pas grave
         elsif params[:type]
             puts "OK                        aaaaaaaaaaa"
-            @current_user = User.find(3);
+            @current_user = User.find(1);
             session = Session.new()
             session.user_id = @current_user.id
             cookies.permanent.signed[:id] = session.user_id
@@ -22,6 +24,24 @@ class ApplicationController < ActionController::Base
             puts "no cookies.signed[:id]"
             @current_user = nil
         end
-    end
-    
+	end
+
+	def send_notification(from_user_id, to_user_id, table_type, table_id, message, status)
+		@notification = Notification.new(
+		  from_user_id: from_user_id,
+		  user_id: to_user_id,
+		  table_type: table_type,
+		  table_id: table_id,
+		  message: message,
+		  status: status
+		)
+		@notification.save
+
+		puts "----- notification ----"
+		puts @notification.to_json
+		puts "-----------------------"
+
+		notif_channel = "notification_channel_" + to_user_id.to_s;
+		ActionCable.server.broadcast(notif_channel, {notification: "On"})
+	end
 end
--- rails/srcs/app/jobs/ask_for_war_timer_job.rb
+++ rails/srcs/app/jobs/ask_for_war_timer_job.rb
@@ -1,20 +1,28 @@
 class AskForWarTimerJob < ApplicationJob
   queue_as :default
 
-  def perform(ask_for_war_id)
-    puts "hello from AskForWarTimerJob, ask_for_war_id = #{ask_for_war_id}"
-  
-    # begin
-    #   
-    # rescue => e
-    #   puts "exception: #{e}"
-    # end
+  def perform(ask_for_war_id, war_time_id, warp_a_id, warp_b_id)
+	@ask_for_game = AskForGame.find(ask_for_war_id)
+	@war_time = WarTime.find(war_time_id)
+	@warp_a = WarParticipation.find(warp_a_id)
+	@warp_b = WarParticipation.find(warp_b_id)
+	to_guild_id = @warp_b.guild_id
+	from_guild_id = @ask_for_game.to_user_id
 
-    # Do something later
-    #
-    #
-    #
+	if (@ask_for_game.status == "pending")
+		@ask_for_game.update( status: "ending" )
 
+		if (@war_time.a == to_guild_id)
+			@war_time.nb_unanswered_call_a += 1
+		else
+			@war_time.nb_unanswered_call_b += 1
+		end
+		@war_time.save
+
+		@warp_a.war_points += 200
+		@warp_a.save
+		#envoyé une notif match gagné en bonus
+	end
   end
 end
 

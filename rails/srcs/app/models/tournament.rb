72736a24b0efe977ea1b51229009eb956ec85675
ylegzouli <ylegzoul@student.42.fr>
2020-11-27 16:20:33 +0100

    ok

--- rails/srcs/app/models/tournament.rb
+++ rails/srcs/app/models/tournament.rb
@@ -4,5 +4,6 @@ class Tournament < ApplicationRecord
     has_many :games, dependent: :nullify
 
     validates :rules, presence: true, length: {minimum: 1}
+    validates :max_nb_player, presence: true, numericality: {greater_than: 2, less_than_or_equal_to: 16, only_integer: true}
     validates :incentives, length: {minimum: 1}, uniqueness: {case_insensitive: false}, allow_blank: true
 end
